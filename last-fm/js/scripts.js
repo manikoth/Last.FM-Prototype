ed76190a8c06504a3bc2eacff176c28a59483950
P. Colin Manikoth <colin@manikoth.com>
2013-02-22 10:24:13 -0800

    1st Prototype
    * This is still confusing.
    * Still trying to understand bootstrap.

--- last-fm/js/scripts.js
+++ last-fm/js/scripts.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
 	$('div#lastfm').lastFM({
 		username: 'ewuweblab',
 		apikey: 'bdafa5aeb85d3c787dd6c9c2f91fc872',
-		number: 10,
+		number: 3,
 		artSize: 'large',
 		onComplete: function(){
 			//Done
-			alert('Done');
+			
 		}
 	});
 
